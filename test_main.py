a4fc1f0a85800acac41335d0e13491ab260b99ea
aquamarine <arturtk000@gmail.com>
2025-09-22 22:15:59 +0300

    123

--- test_main.py
+++ test_main.py
@@ -3,7 +3,6 @@ from main import main
 
 
 def test_main_output(capsys):
-    """Проверка, что main печатает Hello, world!"""
     main()
     captured = capsys.readouterr()
     assert "Hello, world!" in captured.out
